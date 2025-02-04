81eca5d1515e07e8a792e7a439464ad660a30aef
Wolfyxon <wolfyxon@gmail.com>
2025-02-04 16:54:17 +0100

    Reading data from server in client

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,21 @@
-use std::net::TcpStream;
+use std::{io::Read, net::TcpStream};
 
 pub fn join(address: String) {
     let client = TcpStream::connect(address);
 
     match client {
         Ok(mut client) => {
-            println!("Connected");
+            loop {
+                let mut buf: [u8; 256] = [0; 256];
+                
+                match client.read_exact(&mut buf) {
+                    Ok(_) => {
+                        println!("{:?}", buf);
+                    }
+                    
+                    Err(_) => {}
+                }
+            }
         }
         Err(err) => {
             println!("Failed to connect: {err}");
